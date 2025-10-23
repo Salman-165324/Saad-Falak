e5e468b0a62edfd91a22e09c9e3b369f6c6700e8
Salman-165324 <salmansadi165324@gmail.com>
2025-10-23 15:46:27 +0600

    Update Hero component styles for enhanced visibility and layout consistency
    - Increased font sizes for primary and secondary headings to improve emphasis.
    - Adjusted paragraph width for better alignment and readability across different screen sizes.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,14 +8,14 @@ const Hero: React.FC = () => (
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
       <div className="flex flex-row justify-between items-center w-full">
-        <h1 className="flex-1 font-poppins font-semibold ss:text-[40px] text-[24px] text-white ss:leading-[50px] leading-[37px]">
+        <h1 className="flex-1 font-poppins font-semibold ss:text-[42px] text-[24px] text-white ss:leading-[50px] leading-[37px]">
           Your Trusted UK <span className="text-gradient">Business Loan</span>{" "}
         </h1>
       </div>
-      <h1 className="w-full font-poppins font-semibold ss:text-[36px] text-[24px] text-white ss:leading-[50px] leading-[37px]">
+      <h1 className="w-full font-poppins font-semibold ss:text-[38px] text-[24px] text-white ss:leading-[50px] leading-[37px]">
         & Merchant Card Machine Consultant
       </h1>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+      <p className={`${styles.paragraph} max-w-[5200px] mt-5`}>
         With over 6 years of experience in the UK payment and finance industry,
         I help business owners secure the right business loan and card machine
         solutions from trusted providers —
